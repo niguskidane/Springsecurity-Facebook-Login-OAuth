18af993c9464f14b65cf119219c43939e0dd45b6
King <nigus.kidane@gmail.com>
2019-12-08 21:18:22 -0700

    project added

---
+++ src/main/java/io/nk/springsecurityfacebookloginoauth/SpringsecurityFacebookLoginOauthApplication.java
@@ -0,0 +1,13 @@
+package io.nk.springsecurityfacebookloginoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityFacebookLoginOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityFacebookLoginOauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/nk/springsecurityfacebookloginoauth/SpringsecurityFacebookLoginOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nk.springsecurityfacebookloginoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityFacebookLoginOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
