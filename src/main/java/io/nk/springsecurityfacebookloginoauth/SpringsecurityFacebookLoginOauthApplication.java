c7b5f0c3b504dbb880729ea1f1620b89ed15ed3c
King <nigus.kidane@gmail.com>
2019-12-08 23:50:39 -0700

    single-provider single-sign on is added

--- src/main/java/io/nk/springsecurityfacebookloginoauth/SpringsecurityFacebookLoginOauthApplication.java
+++ src/main/java/io/nk/springsecurityfacebookloginoauth/SpringsecurityFacebookLoginOauthApplication.java
@@ -2,8 +2,10 @@ package io.nk.springsecurityfacebookloginoauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class SpringsecurityFacebookLoginOauthApplication {
 
     public static void main(String[] args) {
